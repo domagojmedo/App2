b25ed3260356e91e790b346c07e6bfd146496967
Domagoj Medo <domagoj.medo@moberg.is>
2020-09-22 18:09:16 +0200

    Add project files.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
